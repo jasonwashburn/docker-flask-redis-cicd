753eb34a9bd47612692e6e6bab15017ed6d723fc
Jason Washburn <jason.washburn@gmail.com>
2023-06-06 17:55:15 -0500

    initial app with unit and integration test

---
+++ test/unit/test_app.py
@@ -0,0 +1,28 @@
+
+import unittest.mock as mock
+from redis import ConnectionError
+
+@mock.patch("page_tracker.app.redis")
+def test_should_call_redis_incr(mock_redis, http_client):
+    # Given
+    mock_redis.return_value.incr.return_value = 5
+
+    # When
+    response = http_client.get("/")
+
+    # Then
+    assert response.status_code == 200
+    assert response.text == "This page has been seen 5 times."
+    mock_redis.return_value.incr.assert_called_once_with("page_views")
+
+@mock.patch("page_tracker.app.redis")
+def test_should_return_error_when_redis_is_unavailable(mock_redis, http_client):
+    # Given
+    mock_redis.return_value.incr.side_effect = ConnectionError
+
+    # When
+    response = http_client.get("/")
+
+    # Then
+    assert response.status_code == 500
+    assert response.text == "Sorry, something went wrong \N{pensive face}"
